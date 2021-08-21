3f4e2a694410524a04fdce99ab8e6b762b825c3e
Sarath Kumar M <msarath1@pkrmsezdc.com>
2021-08-21 18:11:29 +0530

    TMOON-7314 Why you should avoid returning lists in APIs

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/controllers/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/controllers/CatalogResource.java
@@ -8,10 +8,8 @@ import org.springframework.web.client.RestTemplate;
 
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
-import com.example.moviecatalogservice.models.Rating;
-
+import com.example.moviecatalogservice.models.UserRating;
 import java.util.List;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,13 +21,14 @@ public class CatalogResource {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		List<Rating> ratingsList = Arrays.asList(new Rating("1234", 3), new Rating("5678", 4));
 
-		return ratingsList.stream().map(rating ->
-		{
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getName(), "Description", rating.getRating());
-        }).collect(Collectors.toList());
+		UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/" + userId,
+				UserRating.class);
+
+		return userRating.getRatings().stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Description", rating.getRating());
+		}).collect(Collectors.toList());
 
 	}
 }
